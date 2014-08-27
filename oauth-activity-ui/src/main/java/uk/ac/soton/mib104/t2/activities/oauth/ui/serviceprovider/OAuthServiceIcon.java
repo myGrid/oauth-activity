b6aabdca1746870ce898db8f23dd9633b80cfea0
Alan R Williams <alan.r.williams@manchester.ac.uk>
2014-08-27 16:39:38 +0100

    Fixed icon

--- oauth-activity-ui/src/main/java/uk/ac/soton/mib104/t2/activities/oauth/ui/serviceprovider/OAuthServiceIcon.java
+++ oauth-activity-ui/src/main/java/uk/ac/soton/mib104/t2/activities/oauth/ui/serviceprovider/OAuthServiceIcon.java
@@ -3,20 +3,15 @@ package uk.ac.soton.mib104.t2.activities.oauth.ui.serviceprovider;
 import java.awt.Color;
 
 import javax.swing.Icon;
-import javax.swing.ImageIcon;
 
-import net.sf.taverna.t2.activities.beanshell.servicedescriptions.BeanshellActivityIcon;
 import net.sf.taverna.t2.workbench.activityicons.ActivityIconSPI;
 import net.sf.taverna.t2.workbench.ui.impl.configuration.colour.ColourManager;
 import net.sf.taverna.t2.workflowmodel.processor.activity.Activity;
-
 import uk.ac.soton.mib104.t2.activities.oauth.OAuth10aRESTActivity;
 import uk.ac.soton.mib104.t2.activities.oauth.OAuthActivity;
 import uk.ac.soton.mib104.t2.activities.oauth.ui.view.IconManager;
 
 public class OAuthServiceIcon implements ActivityIconSPI {
-
-	static Icon icon = null;
 
 	static {
 		final ColourManager colourManager = ColourManager.getInstance();
@@ -36,10 +31,7 @@ public class OAuthServiceIcon implements ActivityIconSPI {
 	}
 
 	public Icon getIcon(final Activity<?> activity) {
-		if (icon == null) {
-			icon = new ImageIcon(BeanshellActivityIcon.class.getResource("/beanshell.png"));
-		}
-		return icon;
+		return IconManager.getSmallIcon();
 	}
 
 }
